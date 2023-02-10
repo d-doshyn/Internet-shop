0ab15b7e40d2daecf7934936df2e546e5ce8123f
d-doshyn <d.doshyn@gmail.com>
2023-02-10 09:44:08 +0200

    add cross to the burger menu

--- js/headerMenu.js
+++ js/headerMenu.js
@@ -1,10 +1,19 @@
 let menuTrigger = document.querySelector('.anchors-burger-menu');
 let burgerMenu = document.querySelector('.anchors-menu');
 
+let menuIcon = document.querySelector('.fa-solid.fa-bars');
+
+function changeIcon(toAdd, toRemove) {
+    menuIcon.classList.add(toAdd);
+    menuIcon.classList.remove(toRemove);
+};
+
 menuTrigger.addEventListener("click", () => {
     if (burgerMenu.style.display == "none") {
         burgerMenu.style.display = "flex";
+        changeIcon("fa-xmark", 'fa-bars');
     } else {
         burgerMenu.style.display = "none";
+        changeIcon('fa-bars', 'fa-xmark');
     }
 });
